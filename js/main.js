a98c09d5fedb3f9cc155da8b89397c5f0da8ebb6
M-Ali-Th <m_ali1426@hotmail.com>
2014-11-25 20:57:49 +0100

    Added submit handler for the deliveryInfo!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(function(){
 
+	//Add submit handler for the deliveryInfo(form)
+	$('.insertDelivery .deliveryForm').submit(function(){
+		var deliveryInfo = {};
+		//Here 'this' is the <form>
+		$(this).find('input').not("input[type='submit']").each(function(){
+			//Here 'this' is the each input on <form>
+			deliveryInfo[this.name] = $(this).val();
+		});
+		//Just to controll it is working
+		console.log('deliveryInfo: ', deliveryInfo);
+
+
+		return false;
+	});
+
 });
