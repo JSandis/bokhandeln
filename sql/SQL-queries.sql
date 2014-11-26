f912b3d805d2a2c77f0339548818320e1d1f08a7
JSandis <johanna_sandis@hotmail.com>
2014-11-26 23:35:23 +0100

    Added sql-question for getting the stock of all books

--- sql/SQL-queries.sql
+++ sql/SQL-queries.sql
@@ -10,4 +10,8 @@
 INSERT INTO delivery (books_isbn, f_price, date, quantity)
 VALUES ({books_isbn}, {f_price}, {date}, {quantity});
 
+# Get the stock of all books (only isbn and quantity)
+SELECT isbn, SUM(quantity) in_stock FROM 
+	(SELECT isbn, quantity FROM deliveries UNION SELECT isbn, -quantity FROM sales) b 
+GROUP BY isbn
 
