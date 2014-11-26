8cceec57d0230e3d36328e42def47be97254f453
M-Ali-Th <m_ali1426@hotmail.com>
2014-11-26 18:32:00 +0100

    Added a sql-question(INSERT INTO) /SQL!

--- sql/SQL-queries.sql
+++ sql/SQL-queries.sql
@@ -2,5 +2,11 @@
 # hostname: 127.0.0.1
 # username: root
 # password: mysql
-# database: sjuansbokhandel
+# database: sjuansbokhandel <-  ???
+
+
+
+#save deliveryInfo to db-delivery
+INSERT INTO delivery (books_isbn, f_price, date, quantity)
+VALUES ({books_isbn}, {f_price}, {date}, {quantity});
 
