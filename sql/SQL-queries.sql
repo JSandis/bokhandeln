e49e03186210c7116c31f05820884503fe4de861
JSandis <johanna_sandis@hotmail.com>
2014-11-27 11:37:56 +0100

    Added some more SLQ-questions

--- sql/SQL-queries.sql
+++ sql/SQL-queries.sql
@@ -5,13 +5,46 @@
 # database: sjuansbokhandel
 
 
+-- FOR DELIVERY FORM
 
-#save deliveryInfo to db-delivery
-INSERT INTO delivery (books_isbn, f_price, date, quantity)
-VALUES ({books_isbn}, {f_price}, {date}, {quantity});
+-- Shelfs will be shown in a drop-down list in form - must have shelfs saved
+# get shelfs
+SELECT * FROM shelfs;
 
-# Get the stock of all books (only isbn and quantity)
+-- END OF FOR DELIVERY FORM
+
+
+-- REGISTER DELIVERY OF A BOOK
+
+# register book
+INSERT INTO books (isbn, title, author)
+VALUES ({isbn}, {title}, {author});
+
+# register delivery
+INSERT INTO deliveries (isbn, f_price, date, quantity)
+VALUES ({isbn}, {f_price}, {date}, {quantity});
+
+# register books shelf
+INSERT INTO book_shelfs (isbn, shelfs_id)
+VALUES ({isbn}, {shelfs_id});
+
+-- END OF REGISTER DELIVERY OF A BOOK
+
+
+-- REGISTER PRICE 
+-- (WHEN REGISTERING DELIVERY OF A BOOK OR ADD A NEW PRICE TO A BOOK)
+# register price
+INSERT INTO books_prices (price, date, isbn)
+VALUES ({price}, {date}, {isbn});
+
+
+-- REGISTER SALE OF A BOOK
+# register sale
+INSERT INTO sales (quantity, date, isbn)
+VALUES ({quantity}, {date}, {isbn});
+
+# get book stock
 SELECT isbn, SUM(quantity) in_stock FROM 
 	(SELECT isbn, quantity FROM deliveries UNION SELECT isbn, -quantity FROM sales) b 
-GROUP BY isbn
+GROUP BY isbn;
 
