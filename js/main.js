3d34804c4af476a284834082db208dc9330e1e9c
M-Ali-Th <m_ali1426@hotmail.com>
2014-11-27 19:34:26 +0100

    Just comment the ajax, I'm not sure about it /JS!

--- js/main.js
+++ js/main.js
@@ -34,23 +34,23 @@ $(function(){
 		});
 
 		//ajax for register (isbn, shelfs_id) to db/book_shelfs
-		$.ajax({
-			url:"libs/sql-ajax-json.php",
-			dataType:"json",
+		// $.ajax({
+		// 	url:"libs/sql-ajax-json.php",
+		// 	dataType:"json",
 
-			data:{
-				sql:"sql/sql-queries.sql",
-				run:"register books shelf",
-				//data to send
-				isbn: JSON.stringify(deliveryInfo.isbn),
-				shelfs_id: JSON.stringify(deliveryInfo.shelfs_id)
-			},
+		// 	data:{
+		// 		sql:"sql/sql-queries.sql",
+		// 		run:"register books shelf",
+		// 		//data to send
+		// 		isbn: JSON.stringify(deliveryInfo.isbn),
+		// 		shelfs_id: JSON.stringify(deliveryInfo.shelfs_id)
+		// 	},
 
-			success: function(data){},
+		// 	success: function(data){},
 
-			error: function(data){}
+		// 	error: function(data){}
 
-		});
+		// });
 
 
 		$.ajax({
