6a9668219651adc3357887b79205e8d0d51c5b4c
M-Ali-Th <m_ali1426@hotmail.com>
2014-11-28 10:10:53 +0100

    Returned the ajax. /JS

--- js/main.js
+++ js/main.js
@@ -25,6 +25,7 @@ $(function(){
 				f_price: JSON.stringify(deliveryInfo.f_price),
 				date: JSON.stringify(deliveryInfo.date),
 				quantity: JSON.stringify(deliveryInfo.quantity)
+
 			},
 
 			success: function(data){},
@@ -34,23 +35,23 @@ $(function(){
 		});
 
 		//ajax for register (isbn, shelfs_id) to db/book_shelfs
-		// $.ajax({
-		// 	url:"libs/sql-ajax-json.php",
-		// 	dataType:"json",
+		$.ajax({
+			url:"libs/sql-ajax-json.php",
+			dataType:"json",
 
-		// 	data:{
-		// 		sql:"sql/sql-queries.sql",
-		// 		run:"register books shelf",
-		// 		//data to send
-		// 		isbn: JSON.stringify(deliveryInfo.isbn),
-		// 		shelfs_id: JSON.stringify(deliveryInfo.shelfs_id)
-		// 	},
+			data:{
+				sql:"sql/sql-queries.sql",
+				run:"register books shelf",
+				//data to send
+				isbn: JSON.stringify(deliveryInfo.isbn),
+				shelfs_id: JSON.stringify(deliveryInfo.shelfs_id)
+			},
 
-		// 	success: function(data){},
+			success: function(data){},
 
-		// 	error: function(data){}
+			error: function(data){}
 
-		// });
+		});
 
 
 		$.ajax({
