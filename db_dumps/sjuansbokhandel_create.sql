71644bc0441211fb47c2f8aefcc761b4ea74a3b1
JSandis <johanna_sandis@hotmail.com>
2014-11-28 13:14:50 +0100

    NEW DB!! Create tables and add data with the newest DB dump!!! Also, changed the create DB SQL

--- db_dumps/sjuansbokhandel_create.sql
+++ db_dumps/sjuansbokhandel_create.sql
@@ -6,78 +6,79 @@ CREATE SCHEMA sjuansbokhandel ;
 -- ---------------------------------------------------------------------------------------------------------
 -- TABLES
 
--- Table: book_shelfs
-CREATE TABLE book_shelfs (
-    isbn bigint(13) NOT NULL,
-    shelfs_id int NOT NULL,
-    CONSTRAINT book_shelfs_pk PRIMARY KEY (isbn)
+-- Table: authors
+CREATE TABLE authors (
+    author_id int    NOT NULL ,
+    first_name varchar(255)    NOT NULL ,
+    last_name varchar(255)    NOT NULL ,
+    CONSTRAINT authors_pk PRIMARY KEY (author_id)
 );
 
 -- Table: books
 CREATE TABLE books (
-    isbn bigint(13) NOT NULL,
-    title varchar(255) NOT NULL,
-    author varchar(255) NOT NULL,
+    isbn bigint(13)    NOT NULL ,
+    title varchar(255)    NOT NULL ,
+    description text    NULL ,
+    author_id int    NOT NULL ,
+    shelf_id int    NOT NULL ,
     CONSTRAINT books_pk PRIMARY KEY (isbn)
 );
 
 -- Table: books_prices
 CREATE TABLE books_prices (
-    id int NOT NULL AUTO_INCREMENT,
-    price decimal(10,2) NOT NULL,
-    date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    isbn bigint(13) NOT NULL,
-    CONSTRAINT books_prices_pk PRIMARY KEY (id)
+    price_id int    NOT NULL  AUTO_INCREMENT,
+    price decimal(10,2)    NOT NULL ,
+    date datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP  ON UPDATE CURRENT_TIMESTAMP ,
+    isbn bigint(13)    NOT NULL ,
+    CONSTRAINT books_prices_pk PRIMARY KEY (price_id)
 );
 
 -- Table: deliveries
 CREATE TABLE deliveries (
-    id int NOT NULL AUTO_INCREMENT,
-    f_price decimal(10,2) NOT NULL,
-    quantity int NOT NULL,
-    date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    isbn bigint(13) NOT NULL,
-    CONSTRAINT deliveries_pk PRIMARY KEY (id)
+    delivery_id int    NOT NULL  AUTO_INCREMENT,
+    f_price decimal(10,2)    NOT NULL ,
+    quantity int    NOT NULL ,
+    date datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP  ON UPDATE CURRENT_TIMESTAMP ,
+    isbn bigint(13)    NOT NULL ,
+    CONSTRAINT deliveries_pk PRIMARY KEY (delivery_id)
 );
 
 -- Table: sales
 CREATE TABLE sales (
-    id int NOT NULL AUTO_INCREMENT,
-    quantity int NOT NULL,
-    date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    isbn bigint(13) NOT NULL,
-    CONSTRAINT sales_pk PRIMARY KEY (id)
+    sale_id int    NOT NULL  AUTO_INCREMENT,
+    quantity int    NOT NULL ,
+    date datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP  ON UPDATE CURRENT_TIMESTAMP ,
+    isbn bigint(13)    NOT NULL ,
+    CONSTRAINT sales_pk PRIMARY KEY (sale_id)
 );
 
--- Table: shelfs
-CREATE TABLE shelfs (
-    id int NOT NULL AUTO_INCREMENT,
-    code char(1) NOT NULL,
-    number int NOT NULL,
-    CONSTRAINT shelfs_pk PRIMARY KEY (id)
+-- Table: shelves
+CREATE TABLE shelves (
+    shelf_id int    NOT NULL  AUTO_INCREMENT,
+    code varchar(4)    NOT NULL ,
+    CONSTRAINT shelves_pk PRIMARY KEY (shelf_id)
 );
 
 
 -- ---------------------------------------------------------------------------------------------------------
 -- FOREIGN KEYS
 
--- Reference:  book_shelfs_books (table: book_shelfs)
-ALTER TABLE book_shelfs ADD CONSTRAINT book_shelfs_books FOREIGN KEY book_shelfs_books (isbn)
-    REFERENCES books (isbn);
-
--- Reference:  book_shelfs_shelfs (table: book_shelfs)
-ALTER TABLE book_shelfs ADD CONSTRAINT book_shelfs_shelfs FOREIGN KEY book_shelfs_shelfs (shelfs_id)
-    REFERENCES shelfs (id);
+-- Reference:  books_authors (table: books)
+ALTER TABLE books ADD CONSTRAINT books_authors FOREIGN KEY books_authors (author_id)
+    REFERENCES authors (author_id);
 
 -- Reference:  books_prices_books (table: books_prices)
 ALTER TABLE books_prices ADD CONSTRAINT books_prices_books FOREIGN KEY books_prices_books (isbn)
     REFERENCES books (isbn);
 
--- Reference:  deliveries_books (table: deliveries)
-ALTER TABLE deliveries ADD CONSTRAINT deliveries_books FOREIGN KEY deliveries_books (isbn)
+-- Reference:  books_shelfs (table: books)
+ALTER TABLE books ADD CONSTRAINT books_shelfs FOREIGN KEY books_shelfs (shelf_id)
+    REFERENCES shelves (shelf_id);
+
+-- Reference:  delivery_books (table: deliveries)
+ALTER TABLE deliveries ADD CONSTRAINT delivery_books FOREIGN KEY delivery_books (isbn)
     REFERENCES books (isbn);
 
 -- Reference:  sales_books (table: sales)
 ALTER TABLE sales ADD CONSTRAINT sales_books FOREIGN KEY sales_books (isbn)
     REFERENCES books (isbn);
-
