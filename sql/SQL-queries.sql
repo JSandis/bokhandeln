d3906056e23f1e7ca00d43ec20b76dd858f96542
JSandis <johanna_sandis@hotmail.com>
2014-11-29 12:02:41 +0100

    Ändrat och lagt SQL-frågor

--- sql/SQL-queries.sql
+++ sql/SQL-queries.sql
@@ -6,58 +6,61 @@
 
 
 -- FOR DELIVERY FORM
-
 -- Shelfs will be shown in a drop-down list in form - must have shelfs saved
-
 # get shelfs
 SELECT * FROM shelfs;
+--
 
--- END OF FOR DELIVERY FORM
+-- AFTER GETTING DATA FROM DELIVERY FORM
+# get isbn from books
+SELECT isbn FROM books WHERE isbn = {isbn};
 
+# get authors
+SELECT author_id FROM authors WHERE first_name = {first_name} AND last_name = {last_name};
 
-
+-- If the author doesn't exist
+# add author
+INSERT INTO authors (first_name, last_name) Output Inserted.IdentityColumnName
+VALUES ({first_name}, {last_name});
+-- DIRECTLY after adding author
+# get author_id
+SELECT LAST_INSERT_ID() AS author_id;
+--
 
 -- REGISTER DELIVERY OF A BOOK
-
 # register book
-INSERT INTO books (isbn, title, author)
-VALUES ({isbn}, {title}, {author});
+INSERT INTO books (isbn, title, description, author_id, shelf_id)
+VALUES ({isbn}, {title}, {description}, {author_id}, {shelf_id});
 
 # register delivery
 INSERT INTO deliveries (isbn, f_price, date, quantity)
 VALUES ({isbn}, {f_price}, {date}, {quantity});
-
-# register books shelf
-INSERT INTO book_shelfs (isbn, shelfs_id)
-VALUES ({isbn}, {shelfs_id});
-
--- END OF REGISTER DELIVERY OF A BOOK
-
-
+-- 
 
 -- REGISTER PRICE 
 -- (WHEN REGISTERING DELIVERY OF A BOOK 
 --	OR ADDING A NEW PRICE TO A BOOK)
+-- We don't have to set the date, it will automatically be the current date and time
 # register price
-INSERT INTO books_prices (price, date, isbn)
-VALUES ({price}, {date}, {isbn});
-
+INSERT INTO books_prices (price, isbn)
+VALUES ({price}, {isbn});
+--
 
 -- REGISTER SALE OF A BOOK
 # register sale
 INSERT INTO sales (quantity, date, isbn)
 VALUES ({quantity}, {date}, {isbn});
-
+--
 
 -- FOR CUSTOMER SEARCH FORM
-
 # get book stock
 SELECT isbn, SUM(quantity) in_stock FROM 
 	(SELECT isbn, quantity FROM deliveries UNION SELECT isbn, -quantity FROM sales) b 
 GROUP BY isbn;
 
--- END OF FOR CUSTOMER SEARCH FORM
+-- 
 
-# search for title and quantity by isbn
-SELECT title, quantity FROM books INNER JOIN deliveries
-on books.isbn = deliveries.isbn;
+
+--# ??
+--SELECT title, quantity FROM books INNER JOIN deliveries
+--on books.isbn = deliveries.isbn;
