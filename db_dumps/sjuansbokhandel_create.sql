93f219e63a05b21ce72d68a54ab8768a3179c96f
JSandis <johanna_sandis@hotmail.com>
2014-11-29 11:21:06 +0100

    Auto increment on id:s. NEW DB! Remove tables and add again with new db dump! All data is there.

--- db_dumps/sjuansbokhandel_create.sql
+++ db_dumps/sjuansbokhandel_create.sql
@@ -8,55 +8,55 @@ CREATE SCHEMA sjuansbokhandel ;
 
 -- Table: authors
 CREATE TABLE authors (
-    author_id int    NOT NULL ,
-    first_name varchar(255)    NOT NULL ,
+    author_id int NOT NULL AUTO_INCREMENT,
+    first_name varchar(255) NOT NULL,
     last_name varchar(255)    NOT NULL ,
     CONSTRAINT authors_pk PRIMARY KEY (author_id)
 );
 
+-- Table: shelves
+CREATE TABLE shelves (
+    shelf_id int NOT NULL AUTO_INCREMENT,
+    code varchar(4) NOT NULL,
+    CONSTRAINT shelves_pk PRIMARY KEY (shelf_id)
+);
+
 -- Table: books
 CREATE TABLE books (
-    isbn bigint(13)    NOT NULL ,
-    title varchar(255)    NOT NULL ,
-    description text    NULL ,
-    author_id int    NOT NULL ,
-    shelf_id int    NOT NULL ,
+    isbn bigint(13) NOT NULL,
+    title varchar(255) NOT NULL,
+    description text NULL,
+    author_id int NOT NULL,
+    shelf_id int NOT NULL,
     CONSTRAINT books_pk PRIMARY KEY (isbn)
 );
 
 -- Table: books_prices
 CREATE TABLE books_prices (
-    price_id int    NOT NULL  AUTO_INCREMENT,
-    price decimal(10,2)    NOT NULL ,
-    date datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP  ON UPDATE CURRENT_TIMESTAMP ,
-    isbn bigint(13)    NOT NULL ,
+    price_id int NOT NULL AUTO_INCREMENT,
+    price decimal(10,2) NOT NULL,
+    date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    isbn bigint(13) NOT NULL,
     CONSTRAINT books_prices_pk PRIMARY KEY (price_id)
 );
 
 -- Table: deliveries
 CREATE TABLE deliveries (
-    delivery_id int    NOT NULL  AUTO_INCREMENT,
-    f_price decimal(10,2)    NOT NULL ,
-    quantity int    NOT NULL ,
-    date datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP  ON UPDATE CURRENT_TIMESTAMP ,
-    isbn bigint(13)    NOT NULL ,
+    delivery_id int NOT NULL AUTO_INCREMENT,
+    f_price decimal(10,2) NOT NULL,
+    quantity int NOT NULL,
+    date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP  ON UPDATE CURRENT_TIMESTAMP,
+    isbn bigint(13) NOT NULL,
     CONSTRAINT deliveries_pk PRIMARY KEY (delivery_id)
 );
 
 -- Table: sales
 CREATE TABLE sales (
-    sale_id int    NOT NULL  AUTO_INCREMENT,
-    quantity int    NOT NULL ,
-    date datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP  ON UPDATE CURRENT_TIMESTAMP ,
+    sale_id int NOT NULL AUTO_INCREMENT,
+    quantity int NOT NULL,
+    date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP  ON UPDATE CURRENT_TIMESTAMP,
     isbn bigint(13)    NOT NULL ,
     CONSTRAINT sales_pk PRIMARY KEY (sale_id)
-);
-
--- Table: shelves
-CREATE TABLE shelves (
-    shelf_id int    NOT NULL  AUTO_INCREMENT,
-    code varchar(4)    NOT NULL ,
-    CONSTRAINT shelves_pk PRIMARY KEY (shelf_id)
 );
 
 
